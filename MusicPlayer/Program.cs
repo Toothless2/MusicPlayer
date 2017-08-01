e114f980f523c984a81c03f4e82777ab6b4d2c10
Toothless <max.rose366@gmail.com>
2017-08-01 14:56:54 +0100

    Initial commit

---
+++ MusicPlayer/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using WMPLib;
+using System.Threading;
+
+namespace MusicPlayer
+{
+    public class Program
+    {
+        public static List<string> unplayedSongs = new List<string>();
+        public static WindowsMediaPlayer currentSong = new WindowsMediaPlayer();
+        static Thread songthread;
+
+        static void Main(string[] args)
+        {
+            AddSongPaths();
+            AddExcludePaths();
+            PlaySong();
+
+
+            //goto play;
+
+            Console.ReadLine();
+        }
+
+        private static void PlaySong()
+        {
+            Random rand = new Random();
+
+            play:
+            //songthread?.Abort();
+            songthread = new Thread(() => PlaySong(unplayedSongs[rand.Next(0, unplayedSongs.Count)]));
+            songthread.Start();
+
+            loop:
+            CleanAndDisplayName();
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "skip" || input == "Skip")
+                {
+                    goto play;
+                }
+
+                if (input == "mute" || input == "mute")
+                {
+                    currentSong.settings.mute = !currentSong.settings.mute;
+                    goto loop;
+                }
+
+                if (input == "pause" || input == "pause")
+                {
+                    currentSong.controls.pause();
+                    goto loop;
+                }
+
+                if (input == "play" || input == "Play")
+                {
+                    currentSong.controls.play();
+                    goto loop;
+                }
+
+                //if (input == "ff")
+                //{
+                //    currentSong.controls.fastForward();
+                //}
+
+                int s = int.MinValue;
+                int.TryParse(input, out s);
+
+                if (s != 0)
+                {
+                    currentSong.settings.volume = int.Parse(input);
+                    goto loop;
+                }
+            }
+        }
+
+        private static void CleanAndDisplayName()
+        {
+            Console.Clear();
+
+            PrintControlls();
+            Console.WriteLine($"currently Playing: {currentSong.currentMedia?.name}");
+
+            if (currentSong.currentMedia == null)
+                CleanAndDisplayName();
+        }
+
+        private static void PrintControlls()
+        {
+            var controlls = "===========\nCommands:\nPause\nPlay\nSkip\nMute\n===========\nType a number for Volume Controll: 1 - 10\n===========";
+            Console.WriteLine(controlls);
+        }
+
+        private static void AddExcludePaths()
+        {
+            Console.WriteLine("Enter Paths to exclude (\"none\" to exclude no paths): ");
+            List<string> exclude = new List<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "none" || input == "done")
+                    break;
+                else
+                    exclude.Add(input);
+            }
+
+            for (int i = 0; i < exclude.Count; i++)
+            {
+                for (int j = unplayedSongs.Count - 1; j >= 0; j--)
+                {
+                    if (unplayedSongs[j].Contains(exclude[i]))
+                        unplayedSongs.RemoveAt(j);
+                }
+            }
+        }
+
+        public static void AddSongPaths()
+        {
+            Console.WriteLine("Enter Music Folder Path: ");
+
+            enterMusicPath:
+            string filePath = Console.ReadLine();
+
+            if (!Directory.Exists(filePath))
+            {
+                Console.WriteLine("Please Enter a valid file path");
+                goto enterMusicPath;
+            }
+
+            string[] paths = Directory.GetFiles(filePath, "*.*", SearchOption.AllDirectories);
+
+            for (int i = 0; i < paths.Length; i++)
+            {
+                if (paths[i] == null)
+                    return;
+
+                if (paths[i].EndsWith(".mp3") || paths[i].EndsWith(".mp4") || paths[i].EndsWith("m4a"))
+                    unplayedSongs.Add(paths[i]);
+            }
+        }
+
+        public static void PlaySong(string path)
+        {
+            currentSong.URL = path;
+            currentSong.settings.volume = 1;
+            currentSong.controls.play();
+
+            while (true)
+            {
+                Thread.Sleep(2000);
+                
+                if((currentSong.controls.currentPosition >= currentSong.currentMedia.duration) || (currentSong.controls.currentPosition <= 0.5))
+                {
+                    PlaySong();
+                }
+            }
+        }
+    }
+}
---
+++ MusicPlayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Simple Music Player")]
+[assembly: AssemblyDescription("A simple music player then will play all misuc files in a specified path")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Toothless")]
+[assembly: AssemblyProduct("MusicPlayer.Properties")]
+[assembly: AssemblyCopyright("2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3b2193a0-040c-4266-8bce-cf572e449d33")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-GB")]
+
