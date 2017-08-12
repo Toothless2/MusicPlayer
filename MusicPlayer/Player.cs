4212edcc155fe09d7468073813af057906314b1d
Toothless <max.rose366@gmail.com>
2017-08-12 18:12:42 +0100

    Added suggestions from stack excahnge code review

--- MusicPlayer/Player.cs
+++ MusicPlayer/Player.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using System.IO;
 using WMPLib;
 using System.Threading;
+using System.Linq;
 
 namespace MusicPlayer
 {
     public class Player
     {
-        public static List<string> unplayedSongs = new List<string>();
-        public static List<string> playedSongs = new List<string>();
-        public static WindowsMediaPlayer currentSong = new WindowsMediaPlayer();
+        private static List<string> unplayedSongs = new List<string>();
+        private static WindowsMediaPlayer currentSong = new WindowsMediaPlayer();
 
         private static Thread songthread;
 
         private static Random rand = new Random();
-
-        private static int count = 0;
+        
         private static int volume = 1;
         private static bool repeat = true;
 
@@ -38,50 +37,55 @@ namespace MusicPlayer
 
         private static void PlaySong()
         {
-            play:
-            songthread?.Abort();
-            songthread = new Thread(() => PlaySong(unplayedSongs[SongLocation()])) {Name = $"PlaySongThread" };
-            songthread.Start();
-
-            loop:
+            StartPlaySongThread();
             CleanAndDisplayName();
 
             while (true)
             {
                 var input = Console.ReadLine();
 
-                if (input == "skip" || input == "Skip")
+                if (string.Equals(input, "skip", StringComparison.InvariantCultureIgnoreCase))
                 {
-                    goto play;
+                    StartPlaySongThread();
+                    CleanAndDisplayName();
                 }
-
-                if (input == "mute" || input == "mute")
+                else if (string.Equals(input, "mute", StringComparison.InvariantCultureIgnoreCase))
                 {
                     currentSong.settings.mute = !currentSong.settings.mute;
-                    goto loop;
+                    CleanAndDisplayName();
                 }
-
-                if (input == "pause" || input == "pause")
+                else if (string.Equals(input, "pause", StringComparison.InvariantCultureIgnoreCase))
                 {
                     currentSong.controls.pause();
-                    goto loop;
+                    CleanAndDisplayName();
                 }
-
-                if (input == "play" || input == "Play")
+                else if (string.Equals(input, "mute", StringComparison.InvariantCultureIgnoreCase))
                 {
                     currentSong.controls.play();
-                    goto loop;
+                    CleanAndDisplayName();
                 }
-
-                int s = int.MinValue;
-                int.TryParse(input, out s);
-
-                if (s != 0)
+                else if(string.Equals(input, "exit", StringComparison.InvariantCultureIgnoreCase))
                 {
-                    volume = currentSong.settings.volume = int.Parse(input);
-                    goto loop;
+                    Environment.Exit(0);
+                }
+                else if(int.TryParse(input, out int s))
+                {
+                    volume = currentSong.settings.volume = s;
+                    CleanAndDisplayName();
+                }
+                else
+                {
+                    CleanAndDisplayName();
+                    Console.WriteLine("Invalid Input please try again");
                 }
             }
+        }
+
+        public static void StartPlaySongThread()
+        {
+            songthread?.Abort();
+            songthread = new Thread(() => PlaySong(unplayedSongs[SongLocation()])) { Name = $"PlaySongThread" };
+            songthread.Start();
         }
 
         private static void CleanAndDisplayName()
@@ -103,51 +107,41 @@ namespace MusicPlayer
 
         private static void AddExcludePaths()
         {
-            Console.WriteLine("Enter Paths to exclude (\"none\" to exclude no paths): ");
+            Console.WriteLine("Enter Paths to exclude (\"none\" to exclude no paths, \"done\" to finish excludeing paths): ");
             List<string> exclude = new List<string>();
 
             while (true)
             {
                 var input = Console.ReadLine();
 
-                if (input == "none" || input == "done")
+                if (string.Equals(input, "none", StringComparison.InvariantCultureIgnoreCase) || string.Equals(input, "done", StringComparison.InvariantCultureIgnoreCase))
                     break;
                 else
                     exclude.Add(input);
             }
 
-            for (int i = 0; i < exclude.Count; i++)
-            {
-                for (int j = unplayedSongs.Count - 1; j >= 0; j--)
-                {
-                    if (unplayedSongs[j].Contains(exclude[i]))
-                        unplayedSongs.RemoveAt(j);
-                }
-            }
+            unplayedSongs.RemoveAll(s => exclude.Any(e => s.Contains(e)));
         }
 
         public static void AddSongPaths()
         {
-            Console.WriteLine("Enter Music Folder Path: ");
+            string filePath = "";
 
-            enterMusicPath:
-            string filePath = Console.ReadLine();
-
-            if (!Directory.Exists(filePath))
+            do
             {
-                Console.WriteLine("Please Enter a valid file path");
-                goto enterMusicPath;
-            }
+                Console.WriteLine("Please inter a valid file path: ");
+                filePath = Console.ReadLine();
+            } while (!Directory.Exists(filePath));
 
             string[] paths = Directory.GetFiles(filePath, "*.*", SearchOption.AllDirectories);
 
-            for (int i = 0; i < paths.Length; i++)
+            foreach (var path in paths)
             {
-                if (paths[i] == null)
+                if (string.IsNullOrEmpty(path))
                     return;
 
-                if (paths[i].EndsWith(".mp3") || paths[i].EndsWith(".mp4") || paths[i].EndsWith("m4a"))
-                    unplayedSongs.Add(paths[i]);
+                if (path.EndsWith(".mp3") || path.EndsWith(".mp4") || path.EndsWith("m4a"))
+                    unplayedSongs.Add(path);
             }
         }
 
@@ -187,6 +181,9 @@ namespace MusicPlayer
         {
             if(unplayedSongs.Count == 0)
             {
+                Console.Clear();
+                Console.WriteLine("Song list compleat exiting");
+
                 songthread.Abort();
                 Environment.Exit(exitCode: 0);
             }
@@ -197,15 +194,13 @@ namespace MusicPlayer
             currentSong.settings.volume = volume;
             currentSong.controls.play();
             CleanAndDisplayName();
-
-            playedSongs.Add(unplayedSongs[location]);
+            
             unplayedSongs.RemoveAt(location);
         }
 
         public static int SongLocation()
         {
-            var location = rand.Next(0, unplayedSongs.Count);
-            return (location == unplayedSongs.Count ? location - 1 : location);
+            return rand.Next(0, unplayedSongs.Count);
         }
     }
 }
